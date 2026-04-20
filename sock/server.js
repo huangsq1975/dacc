71daab86bb66d7c420065f70b04bf9972acb9037
huangsq <huangsq04@gmail.com>
2026-04-20 12:47:02 +0000

    Modify files

---
+++ sock/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`A股K線圖已啟動：http://localhost:${PORT}`);
+});
